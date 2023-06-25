59d5596db6c50636c8e0cde6320b816c85027050
B33 <geoffrey.bii@student.moringaschool.com>
2023-06-25 18:59:34 +0300

    update on introduction and voting

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const main = document.getElementById('main');
 const image = document.getElementById('images')
 const votes = document.getElementById('voting')
+const loader = document.querySelector('loader')
+const intro = document.getElementById('intro');
 
 function fetchData() {
    
@@ -28,27 +30,65 @@ function getData(characters) {
        bt.addEventListener('click', displayAnimalDetails)
        bt.textContent="view"
        p.append(bt)
-       bt.style.color='red'
+             const bt_stylying={
+      display: "block",
+      color: "white",
+      width: "100px",
+      borderRadius: '20px',
+      backgroundColor: '#444',
+      height: "26px"
+      }
+
+//Object.assign():
+      Object.assign(bt.style, bt_stylying);
      
 
      }); 
 
 }
 function displayAnimalDetails(e) {
+  
 const id = e.target.id
   fetchAnimalData(id)      
 }
+
 function createAnimalDetail(data) {
-   image.src = data.image
-   votes.textContent =  data.votes + 'Click to Vote'
-   votes.addEventListener('click', votingP)
+      const head = document.createElement('h3')
+      head.innerContent= 'Here is a cute random dog'
+      intro.append(head)
+      image.src = data.image
+      votes.textContent = ' '+  data.votes + 'Click to Vote'
+      votes.addEventListener('click', votingP)
+      const bt_stylying={
+      display: "block",
+      color: "white",
+      width: "150px",
+      backgroundColor: '#444',
+      alignContent: 'center',
+      height: "30px"
+      }
+      //Object.assign():
+      Object.assign(votes.style, bt_stylying);
+  
 
 }
 function votingP(e) {
+   
     let  vote = Number.parseInt(e.target.textContent) 
     vote += 1
     e.target.textContent = vote ;
+
     
+    const bt_stylying={
+      display: "block",
+      color: "white",
+      width: "100px",
+      borderRadius: '20px',
+      backgroundColor: '#444',
+      height: "26px"
+      }
+      //Object.assign():
+      Object.assign(vote.style, bt_stylying);
 }
 window.onload = fetchData
         
